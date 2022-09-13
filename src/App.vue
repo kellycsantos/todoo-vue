9c981452ea7aa217bdaafdd57396a5b08ee8c4fb
Kelly C Santos <kellycristinasntos@gmail.com>
2022-09-13 00:11:19 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container" :style="theme">
+    <span class="select-theme">
+      Escolha um tema :
+      <select v-model="color">
+        <option v-for="(theme, index) in themes" :key="index" :value="theme">
+          {{ theme.title }}
+        </option>
+      </select>
+    </span>
+
+    <TodoView />
+  </div>
+</template>
+
+<script>
+import TodoView from "./components/TodoView.vue";
+
+export default {
+  components: {
+    TodoView,
+  },
+  data() {
+    return {
+      color: null,
+      themes: [
+        { title: "Hello Jadoo", imgName: "jadoo", color: "#B6E2A3" },
+        { title: "My Melody", imgName: "melo-piano-kuromi", color: "#fed4de" },
+        { title: "Tonari no Totoro", imgName: "totoro", color: "#EEEAE7" },
+        { title: "Nagatoro", imgName: "nagatoro", color: "#FFFFFF" },
+      ],
+      theme: {
+        background: `url(${require(`./assets/background/jadoo.jpg`)}) no-repeat center `,
+      },
+    };
+  },
+  methods: {},
+  mounted() {
+    this.theme.background = ` ${
+      this.themes[0].color
+    } url(${require(`./assets/background/${this.themes[0].imgName}.jpg`)}) no-repeat center `;
+  },
+  watch: {
+    color(val) {
+      this.theme.background = ` ${
+        val.color
+      } url(${require(`./assets/background/${val.imgName}.jpg`)}) no-repeat center `;
+      this.color = val;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+.container {
+  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+.select-theme {
+  align-self: flex-start;
+  margin: 15px 0 5px 5px;
+}
+#app {
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TodoView.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="hello">
+    <div class="input-container">
+      <input
+        type="text"
+        placeholder="Escreva sua tarefa aqui"
+        class="input-task"
+        v-model="newTask"
+      />
+      <input
+        type="submit"
+        @click="addTask"
+        class="button"
+        :disabled="disabled"
+      />
+    </div>
+
+    <div
+      v-for="(task, index) in tasks"
+      :key="index"
+      class="task-row"
+      @click="completedTask(task)"
+    >
+      <span class="status-bar" :style="`background:${task.background}`"></span>
+      <span> {{ task.task }}</span>
+      <i class="fa fa-trash" aria-hidden="true"></i>
+      <span class="status">
+        Finalizada?<input type="checkbox" v-model="task.status" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoView",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      tasks: [],
+      newTask: null,
+      check: true,
+      disabled: true,
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask != null) {
+        this.tasks.push({
+          task: this.newTask,
+          status: false,
+          background: "#ff7878",
+        });
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.newTask = null;
+      }
+    },
+    completedTask(task) {
+      task.status = !task.status;
+
+      if (task.status === true) {
+        task.background = "#acff99";
+      } else {
+        task.background = "#ff7878";
+      }
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    },
+  },
+  watch: {
+    newTask(val) {
+        if (val != null && val.length >= 3) {
+          return (this.disabled = false);
+        } else {
+          this.disabled = true;
+        }
+    },
+  },
+  mounted() {
+    const data = localStorage.getItem("tasks");
+    this.tasks = JSON.parse(data);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+  box-sizing: border-box;
+}
+.hello {
+  width: 80vw;
+  max-width: 600px;
+}
+.input-task {
+  width: 70%;
+  height: 35px;
+  max-width: 500px;
+  margin-right: 5%;
+  padding: 5px;
+}
+
+.button {
+  background: none;
+  outline: none;
+  height: 35px;
+  width: 25%;
+  cursor: pointer;
+}
+
+.button:disabled {
+  cursor: no-drop;
+}
+.input-container {
+  min-height: 5vh;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+.status-bar {
+  display: flex;
+  height: 100%;
+  width: 10px;
+  border-radius: 8px;
+  border: 0.2px solid #00000065;
+}
+
+h1 {
+  margin: 60px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.task-row {
+  border: 1px solid #1a0000;
+  background: rgba(129, 129, 129, 0.116);
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 35px;
+  padding: 2.5px 8px;
+  margin: 15px 0;
+}
+</style>
